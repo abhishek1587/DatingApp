5dddfdf0d66ee813efdb54f076f397993bc847d6
abhishek1587 <abhi_kumar1587@yahoo.co.in>
2021-11-30 13:31:10 +0530

    End of Section 4

--- API/Controllers/UsersContorller.cs
+++ API/Controllers/UsersContorller.cs
@@ -7,9 +7,7 @@ using System.Linq;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
